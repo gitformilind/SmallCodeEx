011f7ea318669c22a6ff36403e24cb6c60e87adb
gitformilind <milind.pangarkar@hotmail.com>
2016-09-24 14:27:43 +0530

    sizeof code

---
+++ sizeof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "*** START ***" << endl;
+  int * p;
+  cout << sizeof(p) << endl;
+  cout << sizeof(int) << endl;
+
+  cout << "*** END ***" << endl;
+}
